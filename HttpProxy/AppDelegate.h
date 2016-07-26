fcfc314d28634849f4d02e335b87cbea9eace36a
Finb <fin.uuid@gmail.com>
2016-07-26 15:31:33 +0800

    Initial Commit

---
+++ HttpProxy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpProxy
+//
+//  Created by huangfeng on 16/7/26.
+//  Copyright © 2016年 Fin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpProxy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpProxy
+//
+//  Created by huangfeng on 16/7/26.
+//  Copyright © 2016年 Fin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
